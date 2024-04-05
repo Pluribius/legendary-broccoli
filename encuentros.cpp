991ecda2d84358b7f673c9094f42912f0d24c790
Rafael <75287800+Pluribius@users.noreply.github.com>
2024-04-05 15:38:29 -0400

    Update encuentros.cpp

--- encuentros.cpp
+++ encuentros.cpp
@@ -1,1 +1,11 @@
 #include "encuentros.h"
+
+encuentros::encuentros(int equipo_a, int puntuacion_a, int equipo_b, int puntuacion_b)
+{
+	//todo validar si el equipo existe y si la puntuacion es mayor o igual a 0 
+	equipoA = equipo_a;
+	puntuacionA = puntuacion_a;
+
+	equipoB = equipo_b;
+	puntuacionB = puntuacion_b;
+}
